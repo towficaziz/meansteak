c5e50a4163df239968cd6c8664f3139a70917c92
unknown <tapolly01@gmail.com>
2022-10-04 21:05:25 +0200

    Transferred multer related code to middleware/file.js from routes/posts.js and clean posts.js file

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,45 +3,27 @@ const express = require("express");
 const checkAuth = require("../middleware/check-auth");
 const PostController = require("../controllers/posts")
 
-const multer = require("multer");
+const extractFile = require("../middleware/file");
+
+// const multer = require("multer");
 
 const router = express.Router();
 
 // this code and 'multer' used for storing file/ images in server
 
-const MIME_TYPE_MAP ={
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg'
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) =>{
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if(isValid){
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename:(req, file, cb) =>{
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '-' + Date.now() + '.' + ext);
-  }
-})
+               //  Multer related codes transfered to file.js in middleware folder
 
 // 'multer' code till here from up
 
 router.post(
   "",
   checkAuth,
-  multer({storage: storage}).single("image"), PostController.createPost);
+  extractFile, PostController.createPost);
 
 router.put(
   "/:id",
   checkAuth,
-  multer({storage: storage}).single("image"), PostController.updatePost);
+  extractFile, PostController.updatePost);
 
 router.get("", PostController.getPosts);
 

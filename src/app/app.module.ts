ebdcaba6da04ce1ae1732805a0118ad0adc62570
unknown <tapolly01@gmail.com>
2022-10-05 19:06:32 +0200

    creating an new module called posts.module.ts and shifts post related components to this file and clean app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import{ HttpClientModule, HTTP_INTERCEPTORS} from'@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
-import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
@@ -16,13 +14,12 @@ import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component'
 
 import { AngularMaterialModule } from './angular-material.module'; // this for ang. material optimization
+import { PostsModule } from './posts/posts.module'; // this for posts component optimization
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent,
     HeaderComponent,
-    PostListComponent,
     SignupComponent,
     LoginComponent,
     ErrorComponent
@@ -33,9 +30,9 @@ import { AngularMaterialModule } from './angular-material.module'; // this for a
     FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ReactiveFormsModule,
 
-    AngularMaterialModule
+    AngularMaterialModule,
+    PostsModule
 
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
---
+++ src/app/posts/posts.module.ts
@@ -0,0 +1,24 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+
+import { PostCreateComponent } from './post-create/post-create.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularMaterialModule } from "../angular-material.module";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations:[
+    PostCreateComponent,
+    PostListComponent,
+  ],
+  imports:[
+    CommonModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    RouterModule
+  ]
+})
+export class PostsModule{
+
+}

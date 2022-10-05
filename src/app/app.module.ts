19fccaef9e8d9adaf7f0e4f0f7431b9fe86ee0c0
unknown <tapolly01@gmail.com>
2022-10-05 18:31:12 +0200

    creating an new module called angular-material.module.ts and shifts all imported angular material modules to that file and cleand app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,8 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatExpansionModule} from '@angular/material/expansion';
 import{ HttpClientModule, HTTP_INTERCEPTORS} from'@angular/common/http';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatDialogModule} from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +14,8 @@ import { LoginComponent } from './auth/login/login.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component'
+
+import { AngularMaterialModule } from './angular-material.module'; // this for ang. material optimization
 
 @NgModule({
   declarations: [
@@ -36,18 +30,13 @@ import { ErrorComponent } from './error/error.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    HttpClientModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
     FormsModule,
-    MatDialogModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+
+    AngularMaterialModule
+
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -17,7 +17,7 @@ export class PostListComponent implements OnInit, OnDestroy
   posts: Post[]=[];
   isLoading = false;
   totalPosts = 0;
-  postsPerPage= 3;
+  postsPerPage= 5;
   currentPage= 1;
   pageSizeOptions=[1, 2, 5, 10];
   userId!: string;
